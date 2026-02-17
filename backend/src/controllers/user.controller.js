a6773ec58163b74559d5e0a3524de463e760629f
shreyanshshardul <nikhilsinghk143@gmail.com>
2026-02-17 17:29:09 +0530

    registered route done

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,8 +1,41 @@
 import {asyncHandler} from  "../utils/AsyncHandler.js";
+import {User} from "../models/user.model.js";
+import {ApiError} from "../utils/ApiError.js";
+import {ApiResponse} from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async (req,res)=>{
-    res.status(200)
-    .json({message:"Ok"})
+
+    const {fullname , email , bio , dob , gender, username,password} = req.body;
+
+    if(!(fullname || email || username || password)){
+        throw new ApiError(404 , "Fill all the details");
+    }
+
+    const existingUser = await User.findOne({
+        $or:[{email} , {username}]
+    });
+    if(existingUser){
+        throw new ApiError(400 , "User already exist")
+    }
+
+     const user = await User.create({
+        fullname,
+        email,
+        bio,
+        dob,
+        gender,
+        username,
+        password
+    })
+
+    const newUser = await User.findById(user._id);
+    if(!newUser){
+        throw new ApiError(400 , "Error while registering the user!!")
+    }
+
+    return res
+    .status(201)
+    .json(new ApiResponse(201 , newUser , "Registered successfully!"));
 })
 
 
--- backend/src/models/User.model.js
+++ backend/src/models/User.model.js
@@ -1,6 +1,7 @@
 import mongoose, { Schema } from "mongoose";
 import bcrypt from "bcrypt";
 
+import jwt from "jsonwebtoken"
 const UserSchema = new Schema(
   {
     fullname: {
@@ -31,19 +32,48 @@ const UserSchema = new Schema(
       type: String,
       maxlength: 200,
     },
+    username:{
+      type:String,
+      unique:true,
+      required:true,
+      lowercase:true,
+    }
   },
   { timestamps: true },
 );
 
-export const User = mongoose.model("User" , UserSchema);
 
-UserSchema.pre("Save" , async function(next){
-    if(!this.isModified("password")) next();
+UserSchema.pre("save" , async function(next){
+    if(!this.isModified("password")) return;
 
-    this.password = bcrypt.hash("this.password" , 10);
-    next();
+    this.password = await bcrypt.hash(this.password , 10);
+     return;
 })
 
 UserSchema.methods.isPasswordCorrect = async function(password){
     return await bcrypt.compare(password , this.password);
 }
+
+UserSchema.methods.generateAccessToken = function(){
+  return jwt.sign({
+    _id :this._id,
+    email:this.email,
+    fullname:this.fullname
+  },
+process.env.ACCESS_TOKEN_SECRET,
+{
+  expireIn:process.env.ACCESS_TOKEN_EXPIRY
+})}
+
+UserSchema.methods.generateRefreshToken = function(){
+  return jwt.sign({
+    _id:this._id,
+  },
+  process.env.REFRESH_TOKEN_SECRET,
+  {
+    expiresIn:process.env.REFRESH_TOKEN_EXPIRY,
+  })}
+  const User = mongoose.model("User" , UserSchema);
+  export {User}
+
+
