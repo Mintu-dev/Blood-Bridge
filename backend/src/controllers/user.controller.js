36ede00e1ad64e8467e8811d5d2451ce0af441b9
shreyanshshardul <nikhilsinghk143@gmail.com>
2026-04-28 14:04:22 +0530

    fix

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,5 +1,5 @@
 import {asyncHandler} from  "../utils/AsyncHandler.js";
-import {User} from "../models/user.model.js";
+import { User } from "../../src/models/user.model.js";
 import {ApiError} from "../utils/ApiError.js";
 import {ApiResponse} from "../utils/ApiResponse.js";
 import Message from "../models/chat.model.js";
