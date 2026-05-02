3075b4474909d178a52f3d8eea0ae77c3dd5555e
shreyanshshardul <nikhilsinghk143@gmail.com>
2026-05-03 01:19:58 +0530

    fixed : Cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -33,7 +33,13 @@ app.use(
 );
 
 // Handle preflight requests
-app.options('*', cors());
+app.options('*', (req, res) => {
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Cookie');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.sendStatus(200);
+});
 
 app.use(express.json());
 app.use(cookieParser());
